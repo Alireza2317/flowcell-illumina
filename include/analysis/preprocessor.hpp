b2d5dff736f8c84010c24028bf3f01259d7ec232
Alireza Alavi <alireza.alavi237@gmail.com>
2026-04-05 20:11:34 +0200

    implemented preprocessor functions

--- include/analysis/preprocessor.hpp
+++ include/analysis/preprocessor.hpp
@@ -3,19 +3,21 @@
 #include <opencv2/opencv.hpp>
 
 namespace analysis {
-	class Preprocessor {
-	  public:
+	namespace prep {
 		struct Config {
 			uint8_t blur_ksize = 3;	  // Gaussian blur kernel size
 			float sigma = 1.0;		  // Gaussian sigma
 			uint8_t morph_ksize = 15; // Size for background estimation
 		};
 
-		explicit Preprocessor(Config cfg = {});
+		class Preprocessor {
+		  public:
+			explicit Preprocessor(Config cfg);
 
-		[[nodiscard]] cv::Mat process(const cv::Mat& input) const;
+			[[nodiscard]] cv::Mat process(const cv::Mat& input) const;
 
-	  private:
-		Config m_cfg;
-	};
+		  private:
+			Config m_cfg;
+		};
+	} // namespace prep
 } // namespace analysis
---
+++ src/analysis/preprocessor.cpp
@@ -0,0 +1,35 @@
+#include "analysis/preprocessor.hpp"
+#include <opencv2/opencv.hpp>
+
+namespace analysis {
+	namespace prep {
+		Preprocessor::Preprocessor(Config cfg) : m_cfg(cfg) {
+		}
+
+		[[nodiscard]] cv::Mat Preprocessor::process(const cv::Mat& input) const {
+			cv::Mat gray;
+			cv::Mat blurred;
+			cv::Mat bg_subtracted;
+
+			// Convert to grayscale
+			if (input.channels() == 3) {
+				cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
+			} else {
+				gray = input.clone();
+			}
+
+			// Denoise
+			cv::GaussianBlur(
+				gray, blurred, cv::Size(m_cfg.blur_ksize, m_cfg.blur_ksize), m_cfg.sigma);
+
+			// Subtract background (morphological top-hat)
+			// Removing low-frequency noise, and keeping blobs
+			cv::Mat kernel = cv::getStructuringElement(
+				cv::MORPH_ELLIPSE, cv::Size(m_cfg.morph_ksize, m_cfg.morph_ksize));
+
+			cv::morphologyEx(blurred, bg_subtracted, cv::MORPH_TOPHAT, kernel);
+
+			return bg_subtracted;
+		}
+	} // namespace prep
+} // namespace analysis
