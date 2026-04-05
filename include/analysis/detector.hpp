75001d60ea965aea71be5d5ead6ddf8cbafa43ae
Alireza Alavi <alireza.alavi237@gmail.com>
2026-04-06 00:32:59 +0200

    added SequenceDecoder header

--- include/analysis/detector.hpp
+++ include/analysis/detector.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/opencv.hpp>
 #include <vector>
 
@@ -23,4 +25,22 @@ namespace analysis {
 		void draw_detections(
 			const cv::Mat& img, cv::Mat& out_img, const std::vector<cv::KeyPoint>& detections);
 	} // namespace detect
+
+	namespace decode {
+		class SequenceDecoder {
+		  public:
+			/// @brief Decodes a single BGR pixel into a base character.
+			/// @param pixel The input pixel to be decoded.
+			/// @return A character representing the base: 'A', 'T', 'C' or 'G'
+			[[nodiscard]] static char pixel2char(const cv::Vec3b& pixel);
+
+			/// @brief Extracts sequences for all detected points across a frame sequence.
+			/// @param coordinates A vector of KeyPoint objects, the actual blob detections.
+			/// @param frames A vector of frame objects, in the order of the sequence.
+			/// @return A vector containing the sequences(as string) for all blobs.
+			[[nodiscard]] std::vector<std::string> decode_all(
+				const std::vector<cv::KeyPoint>& coordinates,
+				const std::vector<cv::Mat>& frames) const;
+		};
+	} // namespace decode
 } // namespace analysis
