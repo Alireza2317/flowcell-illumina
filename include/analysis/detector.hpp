f8595760ba1f6f67de68a1d8aba4c847d18429f9
Alireza Alavi <alireza.alavi237@gmail.com>
2026-04-05 20:59:41 +0200

    defined detector header

--- include/analysis/detector.hpp
+++ include/analysis/detector.hpp
@@ -1,1 +1,29 @@
 #pragma once
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+namespace analysis {
+	namespace detect {
+		/// @brief Represents a detected blob with its center coordinates and size.
+		struct DetectedBlob {
+			cv::Point2f center;
+			float size;
+		};
+
+		/// @brief A class for detecting blobs in an image using OpenCV's SimpleBlobDetector.
+		class BlobDetector {
+		  private:
+			cv::Ptr<cv::SimpleBlobDetector> m_detector;
+
+		  public:
+			/// @brief Constructs a BlobDetector object and initializes the SimpleBlobDetector.
+			BlobDetector();
+			/// @brief Detects blobs in a given processed frame.
+			/// @param processed_frame The input image frame, typically pre-processed.
+			/// grayscale, thresholded, ...
+			/// @return A vector of DetectedBlob objects, each containing the center and size of a
+			/// detected blob.
+			[[nodiscard]] std::vector<DetectedBlob> detect(const cv::Mat& processed_frame) const;
+		};
+	} // namespace detect
+} // namespace analysis
