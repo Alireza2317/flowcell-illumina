0c20963512d69093611d9ce876a3b80294824db5
Alireza Alavi <alireza.alavi237@gmail.com>
2026-04-06 15:53:20 +0200

    modified namespace definitons

--- include/analysis/preprocessor.hpp
+++ include/analysis/preprocessor.hpp
@@ -2,22 +2,20 @@
 #include <cstdint>
 #include <opencv2/opencv.hpp>
 
-namespace analysis {
-	namespace prep {
-		struct Config {
-			uint8_t blur_ksize = 3;	  // Gaussian blur kernel size
-			float sigma = 1.0;		  // Gaussian sigma
-			uint8_t morph_ksize = 15; // Size for background estimation
-		};
+namespace analysis::prep {
+	struct Config {
+		uint8_t blur_ksize = 3;	  // Gaussian blur kernel size
+		float sigma = 1.0;		  // Gaussian sigma
+		uint8_t morph_ksize = 15; // Size for background estimation
+	};
 
-		class Preprocessor {
-		  public:
-			explicit Preprocessor(Config cfg);
+	class Preprocessor {
+	  public:
+		explicit Preprocessor(Config cfg = {});
 
-			[[nodiscard]] cv::Mat process(const cv::Mat& input) const;
+		[[nodiscard]] cv::Mat process(const cv::Mat& input) const;
 
-		  private:
-			Config m_cfg;
-		};
-	} // namespace prep
-} // namespace analysis
+	  private:
+		Config m_cfg;
+	};
+} // namespace analysis::prep
