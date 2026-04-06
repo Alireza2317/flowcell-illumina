45f8c7505a3dc5052da681d54afe1cb4a2becf37
Alireza Alavi <alireza.alavi237@gmail.com>
2026-04-06 15:55:25 +0200

    added a simple test to main
    the test is successful for simpler data, however it struggles with
    more complex data, especially when blobs overlap...
    will be improved soon

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
+#include "analysis/decoder.hpp"
+#include "analysis/detector.hpp"
+#include "analysis/preprocessor.hpp"
+#include "io/frame_loader.hpp"
 #include <filesystem>
+#include <opencv2/opencv.hpp>
 
 const std::filesystem::path ROOT_PATH = PROJECT_ROOT_PATH;
 
-int main() {
+void test(const std::filesystem::path& data_dir) {
+	io::FrameLoader fl(data_dir);
+	const auto raw_frames = fl.get_frames();
 
+	auto preper = analysis::prep::Preprocessor();
+
+	const cv::Mat preped_frame = preper.process(raw_frames.at(0));
+
+	auto detector = analysis::detect::BlobDetector();
+
+	auto detections = detector.detect(preped_frame);
+
+	auto decoder = analysis::decode::SequenceDecoder();
+
+	auto results = decoder.decode_all(detections, raw_frames);
+
+	for (const auto& r : results) {
+		std::cout << r << '\n';
+	}
+}
+
+int main() {
+	test(ROOT_PATH / "data" / "dataset_10cl_7b_20260406_0151");
 	return 0;
 }
